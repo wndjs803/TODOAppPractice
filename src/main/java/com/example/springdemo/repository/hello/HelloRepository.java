7a6426b630ace37a26edcd0f7a3080f6ee6a44fe
김주언 <wndjs803@naver.com>
2025-01-31 17:48:16 +0900

    fix: hello domain 삭제

--- src/main/java/com/example/springdemo/repository/hello/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springdemo.repository.hello;
-
-import com.example.springdemo.dto.UserDto;
-
-public interface HelloRepository {
-    String fetchData();
-    UserDto saveUser(UserDto user);
-    UserDto findUserByName(String name);
-}
--- src/main/java/com/example/springdemo/service/hello/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springdemo.service.hello;
-
-import com.example.springdemo.dto.UserDto;
-
-public interface HelloService {
-    String getHelloMessage();
-    UserDto saveUser(UserDto userDto);
-    UserDto findUserByName(String name);
-}
